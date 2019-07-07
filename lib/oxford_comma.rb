8f166e6e759293226459c324acf2da4c168f1ad9
Alice Brunel <alc.brunel@gmail.com>
2019-07-07 15:08:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,12 @@
+require 'pry'
+
 def oxford_comma(array)
   if array.length < 2
     new_array = array.join(" and ")
   else 
     last = array.pop
     a = array.join(", ")
+    binding.pry
     a.push(" and ")
     a = array << last
   end
